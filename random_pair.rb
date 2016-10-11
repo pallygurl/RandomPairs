7e69c8fe40b134f1ee10251429d60c3b510da683
Cyndi Bolton <cbolton@wvtpg.com>
2016-10-11 14:42:27 -0400

    initial files with submit button

--- random_pair.rb
+++ random_pair.rb
@@ -1,9 +1,6 @@
-# ["lyle", "jacob", "jeremy", "ysabele", "lotfi", "cyndi", "don", "stephanie", "susan", "adam", "mike", "maxx", "john"]
-
-### This is the work everyone just stole form me ###
-def randomizer(names)
-    names_array = names.split(",")
-    shuffled = names_array.shuffle
+def randomizer(user_names)
+    
+    shuffled = user_names.shuffle
     sliced = shuffled.each_slice(2).to_a
     if shuffled.length % 2 == 1
         sliced[-2] << sliced[-1][0]
